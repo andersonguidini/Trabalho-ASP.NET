9869da04f8e02a93443ce67ca2b663d404f3ff7d
Tiago Aparecido <tiagoaparecido007@gmail.com>
2019-12-02 20:25:56 -0300

    DAO de produto, produtoFinal e Receita

--- BakeryManagement/Controllers/ProdutoFinalController.cs
+++ BakeryManagement/Controllers/ProdutoFinalController.cs
@@ -17,55 +17,15 @@ namespace BakeryManagement.Controllers
     {
         private readonly ProdutoFinalDAO _produtoFinalDAO;
 
-        IFirebaseConfig config = new FirebaseConfig
-        {
-            AuthSecret = "l0mQk1Nwesby4YaQUeUPRm87yiOVFTE0q6RX7nW3",
-            BasePath = "https://trabalho-asp.firebaseio.com/"
-        };
-        IFirebaseClient firebase;
-
         public ProdutoFinalController(ProdutoFinalDAO produtoFinalDAO)
         {
             _produtoFinalDAO = produtoFinalDAO;
-            firebase = new FireSharp.FirebaseClient(config);
         }
 
         // GET: ProdutoFinal
         public IActionResult Index()
         {
-            List<ProdutoFinal> produtosFinais = new List<ProdutoFinal>();
-
-            FirebaseResponse reponse = firebase.Get("ProdutoFinal/Counter");
-            String counter = reponse.ResultAs<String>();
-
-//-------------------------------------------------------------------------------------------------//
-
-            if (counter == null)
-            {
-                counter = "0";
-            }
-
-            int cont = 0;
-
-            while (true)
-            {
-                if (cont == Convert.ToInt32(counter))
-                {
-                    break;
-                }
-                cont = cont + 1;
-
-                reponse = firebase.Get("ProdutoFinal/" + cont);
-                ProdutoFinal produtoFinal = reponse.ResultAs<ProdutoFinal>();
-
-                if (produtoFinal != null)
-                {
-                    produtoFinal.Id = Convert.ToInt32(cont);
-                    produtosFinais.Add(produtoFinal);
-                }
-            }
-
-            return View(produtosFinais);
+            return View(_produtoFinalDAO.ListarTodos());
         }
 
         // GET: ProdutoFinal/Create
@@ -77,46 +37,20 @@ namespace BakeryManagement.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(ProdutoFinal produtoFinal, string drpTipo)
         {
-            FirebaseResponse reponse = firebase.Get("ProdutoFinal/Counter");
-            String counter = reponse.ResultAs<String>();
-
-            SetResponse reponseFirebase;
-            ProdutoFinal result;
-
-            if (counter == null)
-            {
-                reponseFirebase = await firebase.SetAsync("ProdutoFinal/Counter", "1");
-                result = reponse.ResultAs<ProdutoFinal>();
-
-                counter = "0";
-            }
-
-            Int32 intCounter = Convert.ToInt32(counter);
-            intCounter = intCounter + 1;
-
-            var data = produtoFinal;
-
-            reponseFirebase = await firebase.SetAsync("ProdutoFinal/" + intCounter, data);
-            result = reponseFirebase.ResultAs<ProdutoFinal>();
-
-            reponseFirebase = await firebase.SetAsync("ProdutoFinal/Counter", Convert.ToString(intCounter));
+            
 
             return RedirectToAction("Index");
         }
 
         public IActionResult Edit(int id)
         {
-            FirebaseResponse reponse = firebase.Get("ProdutoFinal/" + id);
-            ProdutoFinal produtoFinal = reponse.ResultAs<ProdutoFinal>();
-
-            return View(produtoFinal);
+            return View(_produtoFinalDAO.BuscarPorId(id););
         }
 
         [HttpPost]
         public async Task<IActionResult> Edit(ProdutoFinal pf)
         {
-            SetResponse reponseFirebase = await firebase.SetAsync("ProdutoFinal/" + pf.Id, pf);
-            ProdutoFinal result = reponseFirebase.ResultAs<ProdutoFinal>();
+            _produtoFinalDAO.Edit(pf);
 
             return RedirectToAction("Index");
         }
@@ -128,8 +62,7 @@ namespace BakeryManagement.Controllers
                 return NotFound();
             }
 
-            FirebaseResponse reponse = await firebase.DeleteAsync("ProdutoFinal/" + id);
-            ProdutoFinal produtoFinal = reponse.ResultAs<ProdutoFinal>();
+            _produtoFinalDAO.Remover(id);
 
             return RedirectToAction("Index");
         }
--- Repository/ReceitaDAO.cs
+++ Repository/ReceitaDAO.cs
@@ -1,56 +1,128 @@
 ﻿using Domain;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Repository
 {
     public class ReceitaDAO : IRepository<Receita>
     {
-        private readonly Context _context;
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "l0mQk1Nwesby4YaQUeUPRm87yiOVFTE0q6RX7nW3",
+            BasePath = "https://trabalho-asp.firebaseio.com/"
+        };
+        IFirebaseClient firebase;
 
         public ReceitaDAO(Context context)
         {
-            _context = context;
+            firebase = new FireSharp.FirebaseClient(config);
         }
 
         public Receita BuscarPorId(int id)
         {
-            return _context.Receitas.Find(id);
+            FirebaseResponse reponse = firebase.Get("Receita/" + id);
+            Receita receita = reponse.ResultAs<Receita>();
+
+            return receita;
         }
 
         public Receita BuscarPorNome(Receita objeto)
         {
-            return _context.Receitas.FirstOrDefault(x => x.Nome.Equals(objeto.Nome));
+            List<Receita> receitas = ListarTodos();
+
+            foreach (Receita receita in receitas)
+            {
+                if (receita.Nome.Equals(objeto.Nome))
+                {
+                    return receita;
+                }
+            }
+            return null;
         }
 
         public  List<Receita> ListarTodos()
         {
-            return _context.Receitas.ToList();
+            List<Receita> receitas = new List<Receita>();
+
+            FirebaseResponse reponse = firebase.Get("Receita/Counter");
+            String counter = reponse.ResultAs<String>();
+
+            if (counter == null)
+            {
+                counter = "0";
+            }
+
+            int cont = 0;
+
+            while (true)
+            {
+                if (cont == Convert.ToInt32(counter))
+                {
+                    break;
+                }
+                cont = cont + 1;
+
+                reponse = firebase.Get("Receita/" + cont);
+                Receita receita = reponse.ResultAs<Receita>();
+
+                if (receita != null)
+                {
+                    receita.Id = Convert.ToInt32(cont);
+                    receitas.Add(receita);
+                }
+            }
+
+            return receitas;
         }
 
-        public bool Create(Receita objeto)
+        public async Task<bool> Create(Receita receita)
         {
-            if (BuscarPorNome(objeto) == null)
+            if (BuscarPorNome(receita) == null)
             {
-                _context.Receitas.Add(objeto);
-                _context.SaveChanges();
+                FirebaseResponse reponse = firebase.Get("Receita/Counter");
+                String counter = reponse.ResultAs<String>();
+
+                SetResponse reponseFirebase;
+                Receita result;
+
+                if (counter == null)
+                {
+                    reponseFirebase = await firebase.SetAsync("Receita/Counter", "1");
+                    result = reponse.ResultAs<Receita>();
+
+                    counter = "0";
+                }
+
+                Int32 intCounter = Convert.ToInt32(counter);
+                intCounter = intCounter + 1;
+
+                var data = receita;
+
+                reponseFirebase = await firebase.SetAsync("Receita/" + intCounter, data);
+                result = reponseFirebase.ResultAs<Receita>();
+
+                reponseFirebase = await firebase.SetAsync("Receita/Counter", Convert.ToString(intCounter));
                 return true;
             }
             return false;
         }
 
-        public void Remover(int id)
+        public async void Remover(int? id)
         {
-            _context.Receitas.Remove(BuscarPorId(id));
-            _context.SaveChanges();
+            FirebaseResponse reponse = await firebase.DeleteAsync("Receita/" + id);
+            Receita receita = reponse.ResultAs<Receita>();
         }
 
-        public void Editar(Receita objeto)
+        public async void Edit(Receita objeto)
         {
-            _context.Receitas.Update(objeto);
-            _context.SaveChanges();
+            SetResponse reponseFirebase = await firebase.SetAsync("Receita/" + r.Id, r);
+            Receita result = reponseFirebase.ResultAs<Receita>();
         }
     }
 }
