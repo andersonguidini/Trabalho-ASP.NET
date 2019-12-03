426cfed6e90b3268ee03ab0dacc4fe8d187a8dd7
Gabriel Sanches <gabriel.sanches1803@gmail.com>
2019-12-02 22:22:36 -0300

    API

---
+++ API/Controllers/FornecedorAPIController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repository;
+
+namespace API.Controllers
+{
+    [Route("api/Fornecedor")]
+    [ApiController]
+    public class FornecedorAPIController : ControllerBase
+    {
+        private readonly FornecedorDAO _fornecedorDAO;
+
+        public FornecedorAPIController(FornecedorDAO fornecedorDAO)
+        {
+            _fornecedorDAO = fornecedorDAO;
+
+        }
+
+
+        //Get: /api/Fornecedor/BuscarPorNome/{Nome}
+        [HttpGet]
+        [Route("BuscarPorNome/{nome}")]
+        public IActionResult BuscarPorNome([FromRoute]Fornecedor fornecedor)
+        {
+            Fornecedor f = _fornecedorDAO.BuscarPorNome(fornecedor);
+
+            if(f != null)
+            {
+                return Ok(f);
+            }
+            return NotFound(new { msg = "Fornecedor não encontrado!" });
+        }
+
+    }
+}
---
+++ API/Controllers/ProdutoFinalAPIController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repository;
+
+namespace API.Controllers
+{
+    [Route("api/ProdutoFinal")]
+    [ApiController]
+    public class ProdutoFinalAPIController : ControllerBase
+    {
+        private readonly ProdutoFinalDAO _produtoFinalDAO;
+        public ProdutoFinalAPIController(ProdutoFinalDAO produtoFinalDAO)
+        {
+            _produtoFinalDAO = produtoFinalDAO;
+        }
+
+        //Get: /api/ProdutoFinal/BuscarPorId/{id}
+        [HttpGet]
+        [Route("BuscarPorId/{id}")]
+        public IActionResult BuscarPorId([FromRoute]int id)
+        {
+            ProdutoFinal pf = _produtoFinalDAO.BuscarPorId(id);
+            if (pf != null)
+            {
+                return Ok(pf);
+            }
+            return NotFound(new { msg = "Produto Final não encontrado!" });
+        }
+
+    }
+}
