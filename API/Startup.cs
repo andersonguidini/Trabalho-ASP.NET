f2a47d8e1cb1970a2d2005fd8188f1a9d5efbeaf
Anderson Guidini <andersonguidini@gmail.com>
2019-12-09 17:23:36 -0300

    fornecedor por id OK

--- API/Startup.cs
+++ API/Startup.cs
@@ -26,8 +26,8 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<Context>(options => options.UseSqlServer
-                (Configuration.GetConnectionString("BakeryConnection")));
+            //services.AddDbContext<Context>(options => options.UseSqlServer
+            //    (Configuration.GetConnectionString("BakeryConnection")));
 
             services.AddScoped<FornecedorDAO>();
             services.AddScoped<ProdutoFinalDAO>();
